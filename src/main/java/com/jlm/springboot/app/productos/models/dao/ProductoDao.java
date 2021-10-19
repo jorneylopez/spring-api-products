56d7eae5a8ec0046a6fbd70ed783688b3ceadca8
jorneyl <jorney@endoearth.com>
2021-10-18 21:51:13 -0600

    first commit

---
+++ src/main/java/com/jlm/springboot/app/productos/models/dao/ProductoDao.java
@@ -0,0 +1,9 @@
+package com.jlm.springboot.app.productos.models.dao;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.jlm.springboot.app.productos.models.entity.Producto;
+
+public interface ProductoDao extends CrudRepository<Producto, Long>{
+	
+}
---
+++ src/main/resources/import.sql
@@ -0,0 +1,6 @@
+INSERT INTO productos (nombre, precio, create_at) VALUES('Panasonic', 800, NOW());
+INSERT INTO productos (nombre, precio, create_at) VALUES('Sony', 100, NOW());
+INSERT INTO productos (nombre, precio, create_at) VALUES('Apple', 200, NOW());
+INSERT INTO productos (nombre, precio, create_at) VALUES('Nike', 300, NOW());
+INSERT INTO productos (nombre, precio, create_at) VALUES('Dell', 600, NOW());
+INSERT INTO productos (nombre, precio, create_at) VALUES('Sony', 400, NOW());
